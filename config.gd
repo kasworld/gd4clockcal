bf16337e129d51e94d5539c73a927c63fd474322
SeukWon Kang <kasworld@gmail.com>
2023-10-24 16:58:24 +0900

    update version

--- config.gd
+++ config.gd
@@ -10,7 +10,7 @@ var editable_keys = [
 	]
 
 var config = {
-	version_key : "gd4clockcal 3.1.0",
+	version_key : "gd4clockcal 3.2.0",
 	"weather_url" : "http://192.168.0.10/weather.txt",
 	"dayinfo_url" : "http://192.168.0.10/dayinfo.txt",
 	"todayinfo_url" : "http://192.168.0.10/todayinfo.txt",
